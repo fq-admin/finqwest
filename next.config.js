391f18dd3ccf9b5906b90931bfec313dc2c5806a
fq-admin <admin@finquotient.com>
2024-12-24 14:36:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   env: {
-    CLERK_FRONTEND_API: process.env.CLERK_FRONTEND_API, // for the frontend API
     CLERK_API_KEY: process.env.CLERK_API_KEY,           // for server-side API key
   },
 };
